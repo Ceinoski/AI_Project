e526bff31bdebfb0089f8a3c22d01bb9f9aaf1cd
Andres <aec5679@rit.edu>
2023-04-15 16:03:17 -0400

    trial 1

--- CNN_Trial.py
+++ CNN_Trial.py
@@ -1,114 +1,114 @@
 
-# import torch
-# import torch.nn as nn
-# import torch.optim as optim
-# import torch.nn.functional as F
-# import torchvision
-# import torchvision.transforms as transforms
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torchvision
+import torchvision.transforms as transforms
 
-# # Define the CNN architecture
-# class Net(nn.Module):
-#     def __init__(self):
-#         super(Net, self).__init__()
-#         self.conv1 = nn.Conv2d(3, 32, 3, padding=1)
-#         self.conv2 = nn.Conv2d(32, 64, 3, padding=1)
-#         self.conv3 = nn.Conv2d(64, 128, 3, padding=1)
-#         self.pool = nn.MaxPool2d(2, 2)
-#         self.dropout = nn.Dropout(p=0.5)
-#         self.fc1 = nn.Linear(128 * 4 * 4, 512)
-#         self.fc2 = nn.Linear(512, 256)
-#         self.fc3 = nn.Linear(256, 10)
+# Define the CNN architecture
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(3, 32, 3, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, 3, padding=1)
+        self.conv3 = nn.Conv2d(64, 128, 3, padding=1)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.dropout = nn.Dropout(p=0.5)
+        self.fc1 = nn.Linear(128 * 4 * 4, 512)
+        self.fc2 = nn.Linear(512, 256)
+        self.fc3 = nn.Linear(256, 10)
 
-#     def forward(self, x):
-#         x = self.pool(F.relu(self.conv1(x)))
-#         x = self.pool(F.relu(self.conv2(x)))
-#         x = self.pool(F.relu(self.conv3(x)))
-#         x = x.view(-1, 128 * 4 * 4)
-#         x = self.dropout(x)
-#         x = F.relu(self.fc1(x))
-#         x = self.dropout(x)
-#         x = F.relu(self.fc2(x))
-#         x = self.dropout(x)
-#         x = self.fc3(x)
-#         return x
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.pool(F.relu(self.conv3(x)))
+        x = x.view(-1, 128 * 4 * 4)
+        x = self.dropout(x)
+        x = F.relu(self.fc1(x))
+        x = self.dropout(x)
+        x = F.relu(self.fc2(x))
+        x = self.dropout(x)
+        x = self.fc3(x)
+        return x
 
-# def calculate_accuracy(net, dataloader, device):
-#     net.eval()
-#     correct = 0
-#     total = 0
-#     with torch.no_grad():
-#         for data in dataloader:
-#             images, labels = data
-#             images, labels = images.to(device), labels.to(device)
-#             outputs = net(images)
-#             _, predicted = torch.max(outputs.data, 1)
-#             total += labels.size(0)
-#             correct += (predicted == labels).sum().item()
-#     return 100 * correct / total
+def calculate_accuracy(net, dataloader, device):
+    net.eval()
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for data in dataloader:
+            images, labels = data
+            images, labels = images.to(device), labels.to(device)
+            outputs = net(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+    return 100 * correct / total
 
 
-# def main():
-#     net = Net()
+def main():
+    net = Net()
 
-#     # Data preprocessing
-#     transform_train = transforms.Compose(
-#         [transforms.RandomHorizontalFlip(),
-#          transforms.RandomCrop(32, padding=4),
-#          transforms.ToTensor(),
-#          transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    # Data preprocessing
+    transform_train = transforms.Compose(
+        [transforms.RandomHorizontalFlip(),
+         transforms.RandomCrop(32, padding=4),
+         transforms.ToTensor(),
+         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
     
-#     transform_test = transforms.Compose([
-#         transforms.ToTensor(),
-#         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-#     ])
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
 
-#     # Load the CIFAR-10 dataset
-#     trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
-#                                             download=True, transform= transform_train)
-#     trainloader = torch.utils.data.DataLoader(trainset, batch_size=100,
-#                                               shuffle=True, num_workers=2)
+    # Load the CIFAR-10 dataset
+    trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                            download=True, transform= transform_train)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=100,
+                                              shuffle=True, num_workers=2)
 
-#     testset = torchvision.datasets.CIFAR10(root='./data', train=False,
-#                                            download=True, transform=transform_test)
-#     testloader = torch.utils.data.DataLoader(testset, batch_size=100,
-#                                              shuffle=False, num_workers=2)
+    testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                           download=True, transform=transform_test)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=100,
+                                             shuffle=False, num_workers=2)
 
-#     # Define the loss function and the optimizer
-#     criterion = nn.CrossEntropyLoss()
-#     optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9,  weight_decay=5e-4)
-#     scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=10, verbose=True)
+    # Define the loss function and the optimizer
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9,  weight_decay=5e-4)
+    scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=10, verbose=True)
 
 
-#     # Training function
-#     def train(net, dataloader, criterion, optimizer, device):
-#         net.train()
-#         running_loss = 0.0
-#         for i, data in enumerate(dataloader, 0):
-#             inputs, labels = data
-#             inputs, labels = inputs.to(device), labels.to(device)
+    # Training function
+    def train(net, dataloader, criterion, optimizer, device):
+        net.train()
+        running_loss = 0.0
+        for i, data in enumerate(dataloader, 0):
+            inputs, labels = data
+            inputs, labels = inputs.to(device), labels.to(device)
 
-#             optimizer.zero_grad()
+            optimizer.zero_grad()
 
-#             outputs = net(inputs)
-#             loss = criterion(outputs, labels)
-#             loss.backward()
-#             optimizer.step()
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
 
-#             running_loss += loss.item()
-#         return running_loss / (i + 1)
+            running_loss += loss.item()
+        return running_loss / (i + 1)
 
-#     # Train the network
-#     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-#     net.to(device)
+    # Train the network
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    net.to(device)
 
-#     num_epochs = 50
-#     for epoch in range(num_epochs):
-#         train_loss = train(net, trainloader, criterion, optimizer, device)
-#         test_accuracy = calculate_accuracy(net, testloader, device)
-#         scheduler.step(train_loss)
-#         print(f"Epoch {epoch + 1}, Loss: {train_loss}, Test Accuracy: {test_accuracy:.2f}%")
+    num_epochs = 50
+    for epoch in range(num_epochs):
+        train_loss = train(net, trainloader, criterion, optimizer, device)
+        test_accuracy = calculate_accuracy(net, testloader, device)
+        scheduler.step(train_loss)
+        print(f"Epoch {epoch + 1}, Loss: {train_loss}, Test Accuracy: {test_accuracy:.2f}%")
 
-#         # print(f"Epoch {epoch + 1}, Loss: {train_loss}")
+        # print(f"Epoch {epoch + 1}, Loss: {train_loss}")
 
-# if __name__ == '__main__':
-#     main()
+if __name__ == '__main__':
+    main()
